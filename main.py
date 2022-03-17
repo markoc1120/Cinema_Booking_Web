413905bfd7d9ba3baf447a0853421908941ee43d
markoc1120 <markoc1120@gmail.com>
2022-03-17 22:55:24 +0100

    Add home and booking webpage via flask

--- main.py
+++ main.py
@@ -2,16 +2,36 @@ from files.card import Card
 from files.seat import Seat
 from files.user import User
 from files.ticket import Ticket
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, redirect, url_for, flash, abort
 
-name = input('Your full name: ')
-seat = input('Preferred seat: ')
-card_type = input('Your card type: ')
-card_number = input('Your card number: ')
-card_cvc = input('Your card cvc: ')
-card_holder_name = input('Card holder name: ')
 
-user1 = User(name)
-seat1 = Seat('cinema.db', user1, seat)
-card1 = Card('banking.db', card_type, card_number, card_cvc, card_holder_name)
-ticket1 = Ticket(seat1)
-user1.buy(seat1, card1, ticket1)
+
+# name = input('Your full name: ')
+# seat = input('Preferred seat: ')
+# card_type = input('Your card type: ')
+# card_number = input('Your card number: ')
+# card_cvc = input('Your card cvc: ')
+# card_holder_name = input('Card holder name: ')
+#
+# user1 = User(name)
+# seat1 = Seat('cinema.db', user1, seat)
+# card1 = Card('banking.db', card_type, card_number, card_cvc, card_holder_name)
+# ticket1 = Ticket(seat1)
+# user1.buy(seat1, card1, ticket1)
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/booking')
+def booking():
+    return render_template("booking.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
